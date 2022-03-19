c5daf538214e9bafd85ee76b212e396e8931fddc
aaditya <achandrasek3@wisc.edu>
2022-03-19 09:30:20 -0500

    working code: moved settings to main, fixed convg plot

--- Mesher.py
+++ Mesher.py
@@ -4,120 +4,122 @@ import jax.numpy as jnp
 import jax
 
 class RectangularGridMesher:
-    #--------------------------#
-    def __init__(self, ndim, nelx, nely, elemSize, bcSettings):
-        self.meshType = 'gridMesh'
-        self.ndim = ndim;
-        self.nelx = nelx;
-        self.nely = nely;
-        self.elemSize = elemSize;
-        self.bcSettings = bcSettings;
-        self.numElems = self.nelx*self.nely;
-        self.elemArea = self.elemSize[0]*self.elemSize[1]*\
-                        jnp.ones((self.numElems)) # all same areas for grid
-        self.totalMeshArea = jnp.sum(self.elemArea);
-        self.numNodes = (self.nelx+1)*(self.nely+1);
-        self.nodesPerElem = 4; # grid quad mesh
-        self.ndof = self.bcSettings['dofsPerNode']*self.numNodes;
-        self.edofMat, self.nodeIdx, self.elemNodes, self.nodeXY, self.bb = \
-                                        self.getMeshStructure();
-        self.elemCenters = self.generatePoints();
-        self.processBoundaryCondition();
-        self.BMatrix = self.getBMatrix(0., 0.)
-    #--------------------------#
-    def getBMatrix(self, xi, eta):
-      dx, dy = self.elemSize[0], self.elemSize[1];
-      B = np.zeros((3,8));
-      r1 = np.array([(2.*(eta/4. - 1./4.))/dx, -(2.*(eta/4. - 1./4))/dx,\
-                      (2.*(eta/4. + 1./4))/dx,\
-                      -(2.*(eta/4. + 1./4))/dx]).reshape(-1);
-      r2 = np.array([(2.*(xi/4. - 1./4))/dy, -(2.*(xi/4. + 1./4))/dy,\
-                      (2.*(xi/4. + 1./4))/dy, -(2.*(xi/4. - 1./4))/dy])
-      
-      B = [[r1[0], 0., r1[1], 0., r1[2], 0., r1[3], 0.],\
-            [0., r2[0], 0., r2[1], 0., r2[2], 0., r2[3]],\
-            [r2[0], r1[0], r2[1], r1[1], r2[2], r1[2], r2[3], r1[3]]];
-
-      return jnp.array(B)
-    #--------------------------#
-    def getMeshStructure(self):
-        # returns edofMat: array of size (numElemsX8) with
-        # the global dof of each elem
-        # idx: A tuple informing the position for assembly of computed entries
-        n = self.bcSettings['dofsPerNode']*self.nodesPerElem;
-        edofMat=np.zeros((self.nelx*self.nely,n),dtype=int)
-        if(self.bcSettings['dofsPerNode'] == 2): # as in structural
-            for elx in range(self.nelx):
-                for ely in range(self.nely):
-                    el = ely+elx*self.nely
-                    n1=(self.nely+1)*elx+ely
-                    n2=(self.nely+1)*(elx+1)+ely
-                    edofMat[el,:]=np.array([2*n1+2, 2*n1+3, 2*n2+2,\
-                                    2*n2+3,2*n2, 2*n2+1, 2*n1, 2*n1+1]);
-                    
-        elif(self.bcSettings['dofsPerNode'] == 1): # as in thermal
-            nodenrs = np.reshape(np.arange(0, self.ndof), \
-                                 (1+self.nelx, 1+self.nely)).T;
-            edofVec = np.reshape(nodenrs[0:-1,0:-1]+1, \
-                                 self.numElems,'F');
-            edofMat = np.matlib.repmat(edofVec,4,1).T + \
-                  np.matlib.repmat(np.array([0, self.nely+1, self.nely, -1]),\
-                                   self.numElems,1);
-        
-        iK = tuple(np.kron(edofMat,np.ones((n,1))).flatten().astype(int))
-        jK = tuple(np.kron(edofMat,np.ones((1,n))).flatten().astype(int))
-        nodeIdx = jax.ops.index[iK,jK]
+  #--------------------------#
+  def __init__(self, ndim, nelx, nely, elemSize, bcSettings):
+    self.meshType = 'gridMesh'
+    self.ndim = ndim;
+    self.nelx = nelx;
+    self.nely = nely;
+    self.elemSize = elemSize;
+    self.bcSettings = bcSettings;
+    self.numElems = self.nelx*self.nely;
+    self.elemArea = self.elemSize[0]*self.elemSize[1]*\
+                    jnp.ones((self.numElems)) # all same areas for grid
+    self.totalMeshArea = jnp.sum(self.elemArea);
+    self.numNodes = (self.nelx+1)*(self.nely+1);
+    self.nodesPerElem = 4; # grid quad mesh
+    self.ndof = self.bcSettings['dofsPerNode']*self.numNodes;
+    self.edofMat, self.nodeIdx, self.elemNodes, self.nodeXY, self.bb = \
+                                    self.getMeshStructure();
+    self.elemCenters = self.generatePoints();
+    self.processBoundaryCondition();
+    self.BMatrix = self.getBMatrix(0., 0.)
+    self.fig, self.ax = plt.subplots()
+  #--------------------------#
+  def getBMatrix(self, xi, eta):
+    dx, dy = self.elemSize[0], self.elemSize[1];
+    B = np.zeros((3,8));
+    r1 = np.array([(2.*(eta/4. - 1./4.))/dx, -(2.*(eta/4. - 1./4))/dx,\
+                    (2.*(eta/4. + 1./4))/dx,\
+                    -(2.*(eta/4. + 1./4))/dx]).reshape(-1);
+    r2 = np.array([(2.*(xi/4. - 1./4))/dy, -(2.*(xi/4. + 1./4))/dy,\
+                    (2.*(xi/4. + 1./4))/dy, -(2.*(xi/4. - 1./4))/dy])
     
+    B = [[r1[0], 0., r1[1], 0., r1[2], 0., r1[3], 0.],\
+          [0., r2[0], 0., r2[1], 0., r2[2], 0., r2[3]],\
+          [r2[0], r1[0], r2[1], r1[1], r2[2], r1[2], r2[3], r1[3]]];
 
-        elemNodes = np.zeros((self.numElems, self.nodesPerElem));
-        for elx in range(self.nelx):
-            for ely in range(self.nely):
-                el = ely+elx*self.nely
-                n1=(self.nely+1)*elx+ely
-                n2=(self.nely+1)*(elx+1)+ely
-                elemNodes[el,:] = np.array([n1+1, n2+1, n2, n1])
-        bb = {}
-        bb['xmin'],bb['xmax'],bb['ymin'],bb['ymax'] = \
-            0., self.nelx*self.elemSize[0],\
-            0., self.nely*self.elemSize[1]
+    return jnp.array(B)
+  #--------------------------#
+  def getMeshStructure(self):
+    # returns edofMat: array of size (numElemsX8) with
+    # the global dof of each elem
+    # idx: A tuple informing the position for assembly of computed entries
+    n = self.bcSettings['dofsPerNode']*self.nodesPerElem;
+    edofMat=np.zeros((self.nelx*self.nely,n),dtype=int)
+    if(self.bcSettings['dofsPerNode'] == 2): # as in structural
+      for elx in range(self.nelx):
+        for ely in range(self.nely):
+          el = ely+elx*self.nely
+          n1=(self.nely+1)*elx+ely
+          n2=(self.nely+1)*(elx+1)+ely
+          edofMat[el,:]=np.array([2*n1+2, 2*n1+3, 2*n2+2,\
+                          2*n2+3,2*n2, 2*n2+1, 2*n1, 2*n1+1]);
+              
+    elif(self.bcSettings['dofsPerNode'] == 1): # as in thermal
+      nodenrs = np.reshape(np.arange(0, self.ndof), \
+                           (1+self.nelx, 1+self.nely)).T;
+      edofVec = np.reshape(nodenrs[0:-1,0:-1]+1, \
+                           self.numElems,'F');
+      edofMat = np.matlib.repmat(edofVec,4,1).T + \
+            np.matlib.repmat(np.array([0, self.nely+1, self.nely, -1]),\
+                             self.numElems,1);
+    
+    iK = tuple(np.kron(edofMat,np.ones((n,1))).flatten().astype(int))
+    jK = tuple(np.kron(edofMat,np.ones((1,n))).flatten().astype(int))
+    nodeIdx = jax.ops.index[iK,jK]
+
+
+    elemNodes = np.zeros((self.numElems, self.nodesPerElem));
+    for elx in range(self.nelx):
+      for ely in range(self.nely):
+        el = ely+elx*self.nely
+        n1=(self.nely+1)*elx+ely
+        n2=(self.nely+1)*(elx+1)+ely
+        elemNodes[el,:] = np.array([n1+1, n2+1, n2, n1])
+    bb = {}
+    bb['xmin'],bb['xmax'],bb['ymin'],bb['ymax'] = \
+        0., self.nelx*self.elemSize[0],\
+        0., self.nely*self.elemSize[1]
+        
+    nodeXY = np.zeros((self.numNodes, 2))
+    ctr = 0;
+    for i in range(self.nelx+1):
+      for j in range(self.nely+1):
+        nodeXY[ctr,0] = self.elemSize[0]*i;
+        nodeXY[ctr,1] = self.elemSize[1]*j;
+        ctr += 1;
             
-        nodeXY = np.zeros((self.numNodes, 2))
-        ctr = 0;
-        for i in range(self.nelx+1):
-            for j in range(self.nely+1):
-                nodeXY[ctr,0] = self.elemSize[0]*i;
-                nodeXY[ctr,1] = self.elemSize[1]*j;
-                ctr += 1;
-                
-        return edofMat, nodeIdx, elemNodes, nodeXY, bb
-    #--------------------------#
+    return edofMat, nodeIdx, elemNodes, nodeXY, bb
+  #--------------------------#
 
-    def generatePoints(self, res=1):
-        # args: Mesh is dictionary containing nelx, nely, elemSize...
-        # res is the number of points per elem
-        # returns an array of size (numpts X 2)
-        xy = np.zeros((res**2*self.numElems, 2))
-        ctr = 0
-        for i in range(res*self.nelx):
-            for j in range(res*self.nely):
-                xy[ctr, 0] = (i + 0.5)/(res*self.elemSize[0])
-                xy[ctr, 1] = (j + 0.5)/(res*self.elemSize[1])
-                ctr += 1
-        return xy
-    #--------------------------#
-    def processBoundaryCondition(self):
-        force = np.zeros((self.ndof,1))
-        dofs=np.arange(self.ndof)
-        fixed = dofs[self.bcSettings['fixedNodes']]
-        free = np.setdiff1d(np.arange(self.ndof), fixed)
-        force[self.bcSettings['forceNodes']] = self.bcSettings['forceMagnitude']
-        self.bc = {'force':force, 'fixed':fixed,'free':free}
-    #--------------------------#
-    def plotFieldOnMesh(self, field, titleStr):
-        plt.figure()
-        plt.imshow(-np.flipud(field.reshape((self.nelx,self.nely)).T), \
-                   cmap='gray', interpolation='none')
-        plt.axis('Equal')
-        plt.grid(False)
-        plt.title(titleStr)
-        plt.pause(0.01)
+  def generatePoints(self, res=1):
+    # args: Mesh is dictionary containing nelx, nely, elemSize...
+    # res is the number of points per elem
+    # returns an array of size (numpts X 2)
+    xy = np.zeros((res**2*self.numElems, 2))
+    ctr = 0
+    for i in range(res*self.nelx):
+      for j in range(res*self.nely):
+        xy[ctr, 0] = (i + 0.5)/(res*self.elemSize[0])
+        xy[ctr, 1] = (j + 0.5)/(res*self.elemSize[1])
+        ctr += 1
+    return xy
+  #--------------------------#
+  def processBoundaryCondition(self):
+    force = np.zeros((self.ndof,1))
+    dofs=np.arange(self.ndof)
+    fixed = dofs[self.bcSettings['fixedNodes']]
+    free = np.setdiff1d(np.arange(self.ndof), fixed)
+    force[self.bcSettings['forceNodes']] = self.bcSettings['forceMagnitude']
+    self.bc = {'force':force, 'fixed':fixed,'free':free}
+  #--------------------------#
+  def plotFieldOnMesh(self, field, titleStr):
+    plt.ion(); plt.clf()
+    plt.imshow(-np.flipud(field.reshape((self.nelx,self.nely)).T), \
+               cmap='gray', interpolation='none')
+    plt.axis('Equal')
+    plt.grid(False)
+    plt.title(titleStr)
+    plt.pause(0.01)
+    self.fig.canvas.draw()
--- main_TOuNN.py
+++ main_TOuNN.py
@@ -1,52 +1,35 @@
 import numpy as np
-import time
-from Mesher import RectangularGridMesher
-from material import Material
-from TOuNN import TopologyOptimizer
 import matplotlib.pyplot as plt
 from examples import getExampleBC
+from Mesher import RectangularGridMesher
+from projections import computeFourierMap
+from material import Material
+from TOuNN import TOuNN
+from plotUtil import plotConvergence
+#%% Mesh and BC
+ndim, nelx, nely = 2, 40, 20
+elemSize = np.array([1., 1.])
+exampleName, bcSettings, symMap = getExampleBC(2, nelx, nely)
+mesh = RectangularGridMesher(ndim, nelx, nely, elemSize, bcSettings)
 
-ndim = 2; # 2D problem
-nelx = 60; # number of FE elements along X
-nely = 30; # number of FE elements along Y
-elemSize = np.array([1.0,1.0]);
-exampleName, bcSettings, symMap = getExampleBC(1, nelx, nely);
-
-mesh = RectangularGridMesher(ndim, nelx, nely, elemSize, bcSettings);
-
-
-
-exampleName = 'TipCantilever'
-symMap = {'XAxis':{'isOn':False, 'midPt':10.},\
-          'YAxis':{'isOn':False, 'midPt':20.}}
-
-
-matProp = {'physics':'structural', 'E':1.0, 'nu':0.3}
-matProp['penal'] = 1 # SIMP penalization constant, starting value
+#%% Material
+matProp = {'physics':'structural', 'Emax':1.0, 'nu':0.3, 'Emin':1e-3}
 material = Material(matProp)
 
+#%% NN
+nnSettings = {'outputDim':1, 'numNeuronsPerLayer':20,  'numLayers':2}
+fourierMap = {'isOn': True, 'minRadius':4., \
+              'maxRadius':80., 'numTerms':  200}
+fourierMap['map'] = computeFourierMap(mesh, fourierMap)
 
-# For more BCs see examples.py
+#%% Optimization params
+lossMethod = {'type':'penalty', 'alpha0':0.05, 'delAlpha':0.05}
+#lossMethod = {'type':'logBarrier', 't0':3, 'mu':1.1};
+optParams = {'maxEpochs':100, 'learningRate':0.01, 'desiredVolumeFraction':0.5,\
+                     'lossMethod':lossMethod}
 
-nnSettings = {'numLayers':1, 'numNeuronsPerLyr':20 }
-
-fourierMinRadius, fourierMaxRadius = 6, 70;
-numTerms = 100;
-fourierEncoding = {'isOn':True, 'minRadius':fourierMinRadius, \
-              'maxRadius':fourierMaxRadius, 'numTerms':numTerms};
-    
-    
-densityProjection = {'isOn':True, 'sharpness':8};
-desiredVolumeFraction = 0.35;
-
-minEpochs = 150; # minimum number of iterations
-maxEpochs = 500; # Max number of iterations
-    
-plt.close('all');
-overrideGPU = False
-start = time.perf_counter()
-topOpt = TopologyOptimizer(exampleName, mesh, material, nnSettings, symMap,\
-                            fourierEncoding,desiredVolumeFraction, \
-                            densityProjection, overrideGPU);
-topOpt.optimizeDesign(maxEpochs,minEpochs);
-print("Time taken (secs): {:.2F}".format( time.perf_counter() - start))
+#%% Run optimization
+plt.close('all')
+tounn = TOuNN(exampleName, mesh, material, nnSettings, symMap, fourierMap)
+convgHistory = tounn.optimizeDesign(optParams)
+plotConvergence(convgHistory)
